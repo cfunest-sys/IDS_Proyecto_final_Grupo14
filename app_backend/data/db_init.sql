8c9c904ca4f43b14cf1c5c73ff16b8c38524e0dc
Nico <121833029+Ni-coPatt@users.noreply.github.com>
2026-05-19 18:30:58 -0300

    tablas extra (parciales, equipos)

--- app_backend/data/db_init.sql
+++ app_backend/data/db_init.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS alumnos;
 DROP TABLE IF EXISTS profesores;
 DROP TABLE IF EXISTS cursos;
-
+DROP TABLE IF EXISTS logs;
+DROP TABLE IF EXISTS parciales;
+DROP TABLE IF EXISTS equipos;
 
 CREATE TABLE usuarios (
     id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,6 +15,7 @@ CREATE TABLE usuarios (
     contraseña TEXT NOT NULL,
     rol TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE alumnos (
@@ -36,7 +39,27 @@ CREATE TABLE cursos (
     nombre TEXT NOT NULL,
     anio INTEGER NOT NULL,
     semestre INTEGER NOT NULL,
+    id_profesor INT NOT NULL,
 );
+
+CREATE TABLE parciales (
+    id_parcial INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    tipo VARCHAR(100) NOT NULL,
+    fecha DATE NOT NULL,
+    id_curso INT NOT NULL,
+);
+
+CREATE TABLE equipos (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    id_curso INT NOT NULL,
+
+    INDEX (id_curso),
+    FOREIGN KEY (id_curso) REFERENCES cursos (id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO usuarios (email, contraseña, rol) 
 VALUES
@@ -54,5 +77,17 @@ VALUES
 
 INSERT INTO cursos (nombre, anio, semestre)
 VALUES
- ('Introducción al Desarrollo', 2024, 1),
- ('Fundamentos de Programación', 2024, 2);
+ ('Introducción al Desarrollo', 2024, 1, 1),
+ ('Fundamentos de Programación', 2024, 2, 2);
+
+INSERT INTO evaluaciones (nombre, tipo, fecha, id_curso)
+VALUES 
+    ('Primer Parcial Teórico-Práctico', 'parcial', '2026-05-20', 1),
+    ('Trabajo Práctico Integrador Final', 'TP', '2026-06-17', 1),
+    ('Control de Lectura - Parcialito 1', 'parcialito', '2026-05-13', 2);
+
+INSERT INTO equipos (nombre, curso_id)
+VALUES 
+    ('Equipo Artemis 3', 1),
+    ('Equipo Backend Masters', 1),
+    ('Equipo Gastronómico Control', 2);
