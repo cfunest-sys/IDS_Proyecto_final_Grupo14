31115426743a8bbed0a27b31bbcb879be874dc96
Lucca Moyano <lmoyano@fi.uba.ar>
2026-05-19 18:24:12 -0300

    se agrego blueprint evaluaciones, queries y evaluaciones.py

--- app_backend/app.py
+++ app_backend/app.py
@@ -5,6 +5,7 @@ from routes.profesores import profesores_bp
 app = Flask(__name__)
 app.register_blueprint(alumnos_bp, url_prefix='/api/alumnos')
 app.register_blueprint(profesores_bp, url_prefix='/api/profesores')
+app.register_blueprint(evaluaciones_bp, url_prefix='/api/evaluaciones')
 
 @app.route('/')
 def hello_world():
@@ -12,4 +13,3 @@ def hello_world():
 
 if __name__ == '__main__':    
     app.run(debug=True)
-
