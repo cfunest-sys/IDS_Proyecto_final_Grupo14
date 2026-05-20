306aafc04b8d86e373ca599b7d67658463aeba95
cfunest-sys <cfunest@fi.uba.ar>
2026-05-19 23:18:27 -0300

    Enpoint Get equipos en progreso

--- app_backend/routes/alumnos.py
+++ app_backend/routes/alumnos.py
@@ -1,4 +1,6 @@
+from flask import Flask, jsonify, render_template
 from flask import Blueprint
+from database.db import get_connection
 
 alumnos_bp = Blueprint('alumnos', __name__)
 
@@ -11,3 +13,30 @@ def login():
 def register():
     # Hace falta agregar la lógica para el registro de usuarios
     return 'Registro exitoso'
+
+@alumnos_bp.route('/equipos', methods=['GET'])
+def obtener_todos_equipos():
+    conexion = None
+    cursor = None
+
+    try:
+        conexion = get_connection()
+        cursor = conexion.cursor(dictionary=True)
+
+        cursor.execute("SELECT * FROM equipos")
+        equipos = cursor.fetchall()
+
+        if equipos:
+            return render_template('equipos.html', lista_equipos=equipos), 200
+        return render_template('equipos.html', mensaje_error="No hay equipos aún"), 404
+    
+    except Exception as e:
+        print(e)
+        return render_template('error_500.html'), 500
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if conexion:
+            conexion.close()    
+
