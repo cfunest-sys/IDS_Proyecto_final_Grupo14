ebdf9679f1ff8aeca3c038cb23435cd2a02029a4
Luna Alonso Díaz <lalonsod@fi.uba.ar>
2026-05-27 00:50:00 -0300

    Nuevas conexiones

--- app_frontend/routes/routes.py
+++ app_frontend/routes/routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-inicio = Blueprint('inicio', __name__)
+inicio = Blueprint("inicio", __name__)
 
-@inicio.route('/')
+@inicio.route("/")
 def index():
-    return render_template('inicio.html')
+    return render_template("inicio.html")
+
+@inicio.route("/asistencia", methods=["GET", "POST"])
+def asistencia():
+
+    if request.method == "POST":
+
+        alumno = request.form.get("alumno")
+        fecha = request.form.get("fecha")
+    
+    return render_template("asistencia.html")
+
+@inicio.route("/reportes")
+def reportes():
+
+    return render_template("reportes.html")
