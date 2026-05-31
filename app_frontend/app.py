618632aafab5a1e26d34bb706731ecf9bd57dcb6
Lucca Moyano <lmoyano@fi.uba.ar>
2026-05-31 20:46:11 -0300

    agregue filtro por curso id y paginacion a calendario.html

--- app_frontend/app.py
+++ app_frontend/app.py
@@ -19,22 +19,23 @@ def mostrar_alumnos():
 
 @app.route("/calendario")##solamente de prueba
 def mostrar_calendario():
-    eventos = ({"nombre":"tp1", "tipo":"TP", "dia": "21", "curso":1},
-        {"nombre":"Primer Parcial Teórico-Práctico", "tipo":"parcial", "dia": "20", "curso":2},
-        {"nombre":"Trabajo Práctico Integrador Final", "tipo":"TP", "dia": "1", "curso":1},
-        {"nombre":"Control de Lectura - Parcialito 1", "tipo":"parcialito", "dia": "13", "curso":1});
-    mes="febrero"
-    dias=29
-    ##por como lo hice cada vez que se quiera avanzar o retroceder mes hay que llamar al /calendario devuelta
-    ##o el endpoint del front con el que lo renderizemos al calendario.html
-    ##tambien solo hay que pasarle los eventos de ese mes, si diferencia si eran de este mes en jinja era muy engorroso
-    año=2026 
-    bisiesto=False
-    return render_template("calendario.html", mes=mes, dias=dias, año=año, bisiesto=bisiesto,eventos=eventos)
+    eventos = [{"nombre":"tp1", "tipo":"TP", "fecha": "2026-05-21", "curso":1},
+        {"nombre":"Primer Parcial Teórico-Práctico", "tipo":"parcial", "fecha": "2027-12-20", "curso":2},
+        {"nombre":"Trabajo Práctico Integrador Final", "tipo":"TP", "fecha": "2026-05-01", "curso":2},
+        {"nombre":"Control de Lectura - Parcialito 1", "tipo":"parcialito", "fecha": "2026-05-13", "curso":1}]
+    mes_nombre = ("enero","febrero","marzo","abril","mayo","junio","julio","agosto",
+                    "septiembre","octubre","noviembre","diciembre")
+    año_actual = 2026 
+    mes_actual = 5;
+    dias=(31, 28 + año_actual % 4, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    return render_template("calendario.html", mes_actual=mes_actual, dias=dias, año_actual=año_actual, 
+        mes_nombre=mes_nombre,eventos=eventos)
 
 @app.route("/notas")
 def notas():
     return render_template("notas.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
