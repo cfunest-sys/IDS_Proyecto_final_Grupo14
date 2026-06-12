673807cb691dad370c9a744244e3c0d89845a504
Ivan Avillo <iavillo@fi.uba.ar>
2026-06-12 13:31:12 -0300

    resuelvo cosas evaluaciones que no permitian levantanr el docker, toco el front de materiales y cache busting

--- app_frontend/routes/evaluaciones.py
+++ app_frontend/routes/evaluaciones.py
@@ -115,3 +115,30 @@ def crear_evaluacion():
         flash("Error de conexión con el servidor", "danger")
         return redirect(url_for('evaluaciones.listar_evaluaciones'))
     return redirect(url_for('evaluaciones.listar_evaluaciones'))
+
+# 4. RUTA ELIMINAR EVALUACION: http://127.0.0.1:8080/evaluaciones/destruir/<int:id>
+# Acá se crean evaluaciones
+@evaluaciones_blueprint.route("/eliminar", methods=["POST","DELETE"])
+def eliminar_evaluacion():
+    data = {}
+    if (request.form.get("method") != "delete"):
+        flash("Error, intento de conexión indebido", "danger")
+        return redirect(url_for('evaluaciones.listar_evaluaciones'))
+    try:
+        if session:
+            data = {"rol": session.get("rol", ""), "user_id": session.get("user_id", "")}
+            # data = {"rol": "profesor", "user_id": 2}  #--para probar--
+            # data = {"rol": "alumno", "user_id": 2}  #--para probar--
+        data["id"] = request.form.get("id_evaluacion", "")
+        response = requests.delete(f"{current_app.config['BACKEND_URL']}/api/evaluaciones/destruir/{str(data['id'])}")
+        if response.ok:
+            if response.json().get("body", "") != "":
+                json = response.json().get("body", "")
+                if json.get("estado") == True:
+                    flash("Eliminado con éxito","success")
+                else:
+                    flash("No se pudo eliminar", "danger")
+    except requests.exceptions.RequestException:
+        flash("Error de conexión con el servidor", "danger")
+        return redirect(url_for('evaluaciones.listar_evaluaciones'))
+    return redirect(url_for('evaluaciones.listar_evaluaciones'))
