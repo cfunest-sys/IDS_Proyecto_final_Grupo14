1a768dff444777c6d3ea46db836bdd24385b8c11
Joaqofk <jtapiac@fi.uba.ar>
2026-06-24 03:42:33 -0300

    Modifico error visual de fechas en el perfil

--- app_frontend/routes/perfil.py
+++ app_frontend/routes/perfil.py
@@ -18,7 +18,20 @@ def perfil_profesor():
    try:
        response = requests.get(f"{current_app.config['BACKEND_URL']}/api/perfil/", headers=headers, timeout=5)
        if response.ok:
-           datos_perfil = response.json()
+                    datos_perfil = response.json()
+                    MESES = {
+                        'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',
+                        'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'
+                    }
+           
+                    detalles = datos_perfil.get("detalles", {})
+                    for ev in detalles.get("evaluaciones", []):
+                        if isinstance(ev.get("fecha"), str):
+                            partes = ev["fecha"].split(' ')   # ['Mon,', '23', 'Jun', '2025', '00:00:00', 'GMT']
+                            dia = partes[1]
+                            mes = MESES.get(partes[2], '??')
+                            anio = partes[3]
+                            ev["fecha"] = f"{dia}/{mes}/{anio}"
        else:
            flash("Error al cargar el perfil", "danger")
            return redirect("/")
